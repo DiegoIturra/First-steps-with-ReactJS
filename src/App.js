dca1c3bc139a68ca41e7cf46d235b86c4ccb9065
Diego Iturra <diturra@dparadig.com>
2022-11-16 18:08:31 -0300

    SE agregan props al componente de testimonios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,32 @@ function App() {
     <div className="App">
       <div className='main-container'>
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Testimony></Testimony>
+        <Testimony
+          name='Emma Bostian'
+          country='Sweden'
+          image='emma'
+          role='Software Engineer'
+          company='Spotify'
+          testimony='I have always struggled with learning JavaScript. I have taken many courses but freeCodeCamp course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.'
+        />
+
+        <Testimony
+          name='Sarah Chima'
+          country='Nigeria'
+          image='sarah'
+          role='Software Engineer'
+          company='ChatDesk'
+          testimony='freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.'
+        />
+
+        <Testimony
+          name='Shawn Wang'
+          country='Singapore'
+          image='shawn'
+          role='Software Engineer'
+          company='Amazon'
+          testimony='It is scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.'
+        />
       </div>
     </div>
   );
--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import "../styles/Testimony.css"
 
 //All components start with Upper case letter
-function Testimony(){
+function Testimony(props){
   return(
     <div className="testimony-container">
-      <img 
+      <img
         className="testimony-image"
-        src={require('../images/testimony-emma.png')}
+        src={require(`../images/testimony-${props.image}.png`)}
         alt="Emma Photo"
       />
 
+
       <div className="testimony-text-container">
-        <p className="testimony-name">Emma Bostian en Suecia</p>
-        <p className="testimony-role">Ingeniera de Software en Spotify</p>
-        <p className="testimony-text">"I've always struggled with learning JavaScript.
-         I've taken many courses but freeCodeCamp's course was the one which stuck.
-          Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me
-           the skills and confidence
-          I needed to land my dream job as a software engineer at Spotify."</p>
+        <p className="testimony-name"><strong>{props.name}</strong> en {props.country}</p>
+        <p className="testimony-role">{props.role} en <strong>{props.company}</strong></p>
+        <p className="testimony-text">"{props.testimony}"</p>
       </div>
 
     </div>
